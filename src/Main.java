1a45001fc7f14d89234843dcfbe951003fb3729d
ovcych <dranik679@gmail.com>
2023-08-05 22:16:00 +0600

    Done hw 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,38 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello hello");
+        System.out.println(getResult(25, 5));
+        System.out.println(getResult(18, 20));
+        System.out.println(getResult(50, -5));
+        System.out.println(getResult(30, 35));
+        System.out.println(getResult(15, 10));
+
+        test(23, 10);
+        test(generateRandomAge(), 10);
     }
 
+    public static int generateRandomAge() {
+        Random random = new Random();
+        int minAge = 1;
+        int maxAge = 100;
+        return random.nextInt(maxAge - minAge + 1) + minAge;
+    }
+
+    public static void test(int age, int temperature) {
+        String result = getResult(age, temperature);
+        System.out.println("Возраст: " + age + ", температура: " + temperature + " -> " + result);
+    }
+
+    public static String getResult(int age, int temperature) {
+        if (age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30) {
+            return "Можно идти гулять";
+        } else if (age < 20 && temperature >= 0 && temperature <= 28) {
+            return "Можно идти гулять";
+        } else if (age > 45 && temperature >= -10 && temperature <= 25) {
+            return "Можно идти гулять";
+        } else {
+            return "Оставайтесь дома";
+        }
+    }
 }
